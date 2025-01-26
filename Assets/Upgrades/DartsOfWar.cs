e731ca93021b8e55d1474e753e95442a5c4f7a02
Peter Li <peterlimail47@gmail.com>
2025-01-26 14:48:32 -0800

    saving

--- Assets/Upgrades/DartsOfWar.cs
+++ Assets/Upgrades/DartsOfWar.cs
@@ -5,6 +5,7 @@ public class DartsOfWar : UpgradeBase
     public override void onUpgrade()
     {
         GameManager.Instance.mouseBurnRadius = 1.0f;
+        GameManager.Instance.mouseBurnInterval = 0.05f;
         GameManager.Instance.mouseBurnTargetCount = 2;
     }
 }
