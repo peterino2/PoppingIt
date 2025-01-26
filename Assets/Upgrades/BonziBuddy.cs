c5353fb1898c75cb38b0b3ce17b0f5f1a08bdfa5
Peter Li <peterlimail47@gmail.com>
2025-01-26 12:30:58 -0800

    some upgrades

---
+++ Assets/Upgrades/BonziBuddy.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class BonziBuddy : UpgradeBase
+{
+    public GameObject bonziBuddyObject;
+    public override void onUpgrade()
+    {
+        GameManager.Instance.randomPopperRate = 2.0f;
+        bonziBuddyObject.gameObject.SetActive(true);
+    }
+}
