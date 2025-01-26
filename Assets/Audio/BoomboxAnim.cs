e4e55c4b2029d13042382f78b6a2591c4a5c1864
Peter Li <peterlimail47@gmail.com>
2025-01-25 17:26:52 -0800

    boombox anim based on samples

--- Assets/Audio/BoomboxAnim.cs
+++ Assets/Audio/BoomboxAnim.cs
@@ -20,7 +20,9 @@ public class BoomboxAnim : MonoBehaviour
     void Update()
     {
         float playtime = AudioSystem.get().bgmPlayTime;
-        float skew = AudioSystem.get().skewCurve.Evaluate(playtime);
+        // float skew = AudioSystem.get().skewCurve.Evaluate(playtime);
+        float skew = AudioSystem.get().clipLoudness * 10;
         rtrans.rotation = Quaternion.AngleAxis(skew * intensity, Vector3.forward);
+        rtrans.localScale = Vector3.one * (skew * 0.3f + 1.0f);
     }
 }
